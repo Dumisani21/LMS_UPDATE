9063d81978b76d181f15de628590a447dc0e2544
wethinkcode_ <wethinkcode_@pop-os.localdomain>
2023-05-17 17:03:44 +0200

    lms tool update function done

--- update_wtc-lms.py
+++ update_wtc-lms.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 
 
+
 def check_version() -> str:
     command = "wtc-lms --version"
     output = subprocess.check_output(command, shell=True, text=True)
@@ -16,43 +17,46 @@ def is_int(value) -> bool:
 
 def update_lms(path_location="Downloads/") -> bool:
     user = os.getlogin()
-    path_exists = os.path.exists(os.path.join("/home",user,path_location))
-    path = os.path.join("/home",user,path_location)
-    downloaded_file = os.path.exists(f"{path}wtc-lms")
-    installed_file = os.path.exists(os.path.join("/usr","bin","wtc-lms"))
-    if downloaded_file and installed_file:
-        check_current_lms = subprocess.check_output("wtc-lms --version", shell=True, text=True)
-        check_downloaded_lms = subprocess.check_output(f"source {path}wtc-lms --version")
-    else:
-        if not downloaded_file:
-            print("it seems like you don't have wtc-lms downloaded yet!")
-        if not installed_file:
-            print("it seems like you don't have wtc-lms installed yet!")
-        return
-    
+    path = os.path.join("/home", user, path_location)
+    downloaded_file = os.path.exists(os.path.join(path, "wtc-lms"))
+    installed_file = os.path.exists(os.path.join("/usr/bin", "wtc-lms"))
+
+    if not downloaded_file:
+        print("It seems like you don't have wtc-lms downloaded yet!")
+        return False
+
+    if not installed_file:
+        print("It seems like you don't have wtc-lms installed yet!")
+        return False
+
+    check_current_lms = check_version().strip()
+    os.system(f"chmod +x {path}wtc-lms")
+    check_downloaded_lms = subprocess.check_output(f"{path}wtc-lms --version", shell=True, text=True).strip()
+
     if check_current_lms == check_downloaded_lms:
         print("lms is already up to date!")
-        return
+        return True
     else:
+        path_exists = os.path.exists(path)
         if path_exists:
             command = "whereis wtc-lms"
             output = subprocess.check_output(command, shell=True, text=True).strip().split(" ")
             output.pop(0)
             if len(output) > 1:
                 for i in range(len(output)):
-                    print(f"[{i+1}]: {output}")
-                selection = input(f"Enter selection: ")
+                    print(f"[{i+1}]: {output[i]}")
+                selection = input("Enter selection: ")
                 while not is_int(selection):
                     print("Please enter numbers only")
                     if is_int(selection):
-                        if int(selection) > output:
+                        if int(selection) > len(output):
                             print("Please enter the numbers that appear only!")
                             continue
                     selection = input("Enter selection: ")
-                output = output[selection]
+                output = output[int(selection) - 1]
             else:
                 output = output[0]
-            print(f"is this your primary location: {output} (y)es/(n)o?")
+            print(f"Is this your primary location: {output} (y)es/(n)o?")
             response = ""
             while response == "":
                 response = input("> ").strip().lower()
@@ -62,20 +66,25 @@ def update_lms(path_location="Downloads/") -> bool:
                     copy = "/".join(copy)
                     if os.path.exists(f"{path}wtc-lms"):
                         os.system(f"cp {path}wtc-lms {copy}")
-                        check_version = subprocess.check_output("wtc-lms --version",shell=True, text=True)
-                        check_current = subprocess.check_output(f"{path}wtc-lms --version")
-                        if check_version == check_current:
-                            print("lms updated :\)")
+                        current_version = check_version().strip()
+                        check_current = subprocess.check_output("wtc-lms --version", shell=True, text=True).strip()
+            
+                        if current_version == check_current:
+                            print("lms updated :)")
+                            print(f"Version: {current_version}")
+                            return True
+                        else:
+                            print("Failed to update lms!")
+                            return False
                     else:
-                        print("Please download wtc-lms first then run this script again")
-                        return
+                        print("Please download wtc-lms first, then run this script again")
+                        return False
                 elif response == "n":
                     print("lms not updated")
-                    return
-
+                    return False
         else:
             print("Path does not exist!")
-    return True
+            return False
 
 
 update_lms()
